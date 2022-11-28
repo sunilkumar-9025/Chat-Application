c1e4261d5e319970e9b43973d7bfc66ab05cfa2b
Sunil Kumar <108066840+sunilkumar-9025@users.noreply.github.com>
2022-11-28 11:52:03 +0530

    Add files via upload

---
+++ chat-application/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+  return (
+    <div className="App">
+      <ChatEngine
+        height="100vh"
+        projectID="bb3d9f18-ab9c-40e2-a26c-03d652938bfa"
+        userName={localStorage.getItem("username")}
+        userSecret={localStorage.getItem("password")}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() =>
+          new Audio(
+            "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+          ).play()
+        }
+      />
+    </div>
+  );
+}
+
+export default App;
